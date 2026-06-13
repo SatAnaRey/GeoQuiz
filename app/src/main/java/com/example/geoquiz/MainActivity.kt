2f3c267ced0325641d680db12a7445931e856556
SatAnaRey <kurousagi.san@yandex.ru>
2026-06-13 21:35:17 +0700

    Добавить каркас QuizScreen

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.geoquiz.ui.theme.GeoQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GeoQuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+                    QuizScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -28,14 +36,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
+fun QuizScreen(modifier: Modifier = Modifier) {
+    val question = questionBank[0]  // временно только первый вопрос
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = question.text,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(vertical = 24.dp)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = { }) { Text("TRUE") }
+            Button(onClick = { }) { Text("FALSE") }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun QuizScreenPreview() {
     GeoQuizTheme {
-        Greeting("Android")
+        QuizScreen()
     }
 }
